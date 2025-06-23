66f01e8e172d943e77a35449581c5788ae261c46
vais2004 <vkawale2004@gmail.com>
2025-06-23 22:04:27 +0530

    update: header removing extra commented code

--- my-app/src/components/Header.jsx
+++ my-app/src/components/Header.jsx
@@ -14,38 +14,3 @@ const Header = () => {
 };
 
 export default Header;
-// import React from "react";
-// import { Link } from "react-router-dom";
-
-// const Header = () => {
-//   return (
-//     <header className="container">
-//       <nav className="navbar navbar-expand-lg bg-light py-3">
-//         <div className="container-fluid">
-//           <Link className="navbar-brand fw-bold fs-1" to="/">
-//             Anvaya
-//           </Link>
-//           <div className="collapse navbar-collapse">
-//             <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-//               <li className="nav-item">
-//                 <Link className="nav-link" to="/">Dashboard</Link>
-//               </li>
-//               <li className="nav-item">
-//                 <Link className="nav-link" to="/lead/management">Leads</Link>
-//               </li>
-//               <li className="nav-item">
-//                 <Link className="nav-link" to="/agent/management">Agents</Link>
-//               </li>
-//               <li className="nav-item">
-//                 <Link className="nav-link" to="/reports">Reports</Link>
-//               </li>
-//             </ul>
-//           </div>
-//         </div>
-//       </nav>
-//     </header>
-//   );
-// };
-
-// export default Header;
-
